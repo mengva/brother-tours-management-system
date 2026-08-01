0376f09fd69b48ba4490f7a813f477b695c0eb9b
meejvaj <mengvaprogamemin@gmail.com>
2026-08-01 22:43:18 +0700

    modify package

--- apps/web-admin/next.config.ts
+++ apps/web-admin/next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
+  transpilePackages: ["@workspace/ui", "@/server/packages"],
   eslint: {
     ignoreDuringBuilds: true,
   },
--- apps/web-landing/next.config.ts
+++ apps/web-landing/next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next"
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@workspace/ui"],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@workspace/ui", "@/server/packages"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Disable the 'x-powered-by: Next.js' header for security
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      }
+    ],
+    domains: ['localhost'],
+    unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ["react", "lucide-react", "recharts"],
+  },
+  // Enable React Strict Mode for enhanced development checks
+  reactStrictMode: true
 }
 
 export default nextConfig
