5b3d0cfde119ade5a03d4111ea6ffbb441f730ba
meejvaj <mengvaprogamemin@gmail.com>
2026-08-01 23:40:20 +0700

    modify port

--- apps/web-landing/app/trpc/Provider.tsx
+++ apps/web-landing/app/trpc/Provider.tsx
@@ -24,7 +24,7 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
         }),
         httpBatchLink({
           transformer: superjson,
-          url: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:9876'}/trpc`,
+          url: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5050'}/trpc`,
           fetch(url, options) {
             return fetch(url, {
               ...options,
